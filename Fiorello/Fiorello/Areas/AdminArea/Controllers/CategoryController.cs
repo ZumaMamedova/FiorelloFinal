65d31e304540cec21b015c1ae4034efd929f22d8
ZumaMamedova <zumrudam@code.edu.az>
2023-06-15 22:11:09 +0400

    update

--- Fiorello/Fiorello/Areas/AdminArea/Controllers/CategoryController.cs
+++ Fiorello/Fiorello/Areas/AdminArea/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Fiorello.DAL;
+using Fiorello.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fiorello.Areas.AdminArea.Controllers
@@ -16,6 +17,25 @@ namespace Fiorello.Areas.AdminArea.Controllers
         public IActionResult Index()
         {
             return View(_appDbContext.Categories.ToList());
+        }
+
+        public IActionResult Detail(int?id)
+        {
+            if(id == null) return NotFound();
+            var category = _appDbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            return Content(category.Name+" "+category.Desc);
         }
     }
 }
